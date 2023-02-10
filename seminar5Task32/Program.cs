15853612386d1da6813f1aec4780f2d8d8ba5590
Almaz <minnexanov.a@mail.ru>
2023-02-10 15:50:05 +0300

    seminar5Task32/Program.cs

--- seminar5Task32/Program.cs
+++ seminar5Task32/Program.cs
@@ -2,26 +2,29 @@
 //положительные элементы замените на соответствующие 
 //отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
 Console.Clear();
+
+int size = InputNumber("Введите размер массива");
+int minValue = InputNumber("Введите минимальное значение в массиве");
+int maxValue = InputNumber("Введите максимальное значение в массиве");
+int[] array = GetArray(size, minValue, maxValue );
+
+
 int [] GetArray (int size, int minValue, int maxValue )
 {
     int[] res = new int[size];
     for(int i=0; i<size; i++)
     {
-        res[i] = new Random().Next(minValue, (maxValue+1));
-       
+        res[i] = new Random().Next(minValue, (maxValue+1));       
+        //if (minValue < 0) minValue = minValue*(-1);
+        //if (maxValue < 0) minValue = minValue*(-1);
     }
     return res;
 }
 
-
-int size = InputNumber("Введите размер массива");
-int minValue = InputNumber("Введите минимальное значение в массиве");
-int maxValue = InputNumber("Введите максимальное значение в массиве");
-int[] barray = GetArray(size, minValue, maxValue );
-
-barray = GetArray(size, minValue, maxValue );
-Console.WriteLine("изначально ["+String.Join(", ",(barray))+"]");
+array = GetArray(size, minValue, maxValue );
+Console.WriteLine("сгенерированный массив из случайных чисел ["+String.Join(", ",(array))+"]");
 
 
 int[] ChangeSign(int[] someArray)
@@ -29,17 +32,14 @@ int[] ChangeSign(int[] someArray)
     int[] res = someArray;
     for (int i = 0; i <= res.Length - 1; i++) res[i] *= (-1);
     return res;
-    //Console.Write("["+String.Join(", ",(someArray))+"]");
-    // Console.WriteLine();
 }
 
 int InputNumber(string message)
 {
-    //Выводит предложение message и в ответе получает число. Проверку не производит
     Console.Write($"{message}: ");
     int res = int.Parse(Console.ReadLine()!);
     return res;
 }
 
-barray = ChangeSign(barray);
-Console.Write("потом ["+String.Join(", ",(barray))+"]");
+array = ChangeSign(array);
+Console.Write("измененные элементов на противоположные ["+String.Join(", ",(array))+"]");
