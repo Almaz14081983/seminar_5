cd9e18a8ca10a2bb4a6c72af73ef43a2c7550386
Almaz <minnexanov.a@mail.ru>
2023-02-10 15:09:55 +0300

    seminar5Task32/Program.cs

--- seminar5Task32/Program.cs
+++ seminar5Task32/Program.cs
@@ -2,56 +2,44 @@
 //положительные элементы замените на соответствующие 
 //отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2] 
-
-//Задача 32: Напишите программу замена элементов массива: положительны
-//элементы замените на соответствующие отрицательные, и наоборот.
-//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
-
-//-------
-//Ввод исходных данных
-int size = InputNumber("Введите размер массива");
-int min = InputNumber("Введите минимальное значение в массиве");
-int max = InputNumber("Введите максимальное значение в массиве");
-int[] array = GetArray(size, min, max);
-
-//--------
-//Работаем
 Console.Clear();
-PrintArray(array);
-array = ChangeSign(array);
-PrintArray(array);
+int [] GetArray (int size, int minValue, int maxValue )
+{
+    int[] res = new int[size];
+    for(int i=0; i<size; i++)
+    {
+        res[i] = new Random().Next(minValue, (maxValue+1));
+       
+    }
+    return res;
+}
 
-//---------
-//Методы
+
+int size = InputNumber("Введите размер массива");
+int minValue = InputNumber("Введите минимальное значение в массиве");
+int maxValue = InputNumber("Введите максимальное значение в массиве");
+int[] barray = GetArray(size, minValue, maxValue );
+
+barray = GetArray(size, minValue, maxValue );
+Console.WriteLine("изначально ["+String.Join(", ",(barray))+"]");
+
+
 int[] ChangeSign(int[] someArray)
 {
-    //Меняет знак у элементов массива
     int[] res = someArray;
     for (int i = 0; i <= res.Length - 1; i++) res[i] *= (-1);
     return res;
+    //Console.Write("["+String.Join(", ",(someArray))+"]");
+    // Console.WriteLine();
 }
-int[] GetArray(int length, int min = -1, int max = 1)
-{
-    //Генерирует массив целых чисел заданной длинны length
-    //По-умолчанию числа в массиве от -1 до +1, меняется аргументами min и max
-    int[] res = new int[length - 1];
-    for (int i = 0; i < length; i++) res[i] = new Random().Next(min, max + 1);
-    return res;
-}
-void PrintArray(int[] someArray)
-{
-    //Выводит массив в консоль в виде [,,]
-    Console.Write("[");
-    for (int i = 0; i <= someArray.Length - 1; i++)
-    {
-        Console.Write($"{someArray[i]}, ");
-    }
-    Console.WriteLine($"{someArray[someArray.Length - 1]}]");
-}
+
 int InputNumber(string message)
 {
     //Выводит предложение message и в ответе получает число. Проверку не производит
     Console.Write($"{message}: ");
-    int res = int.Parse(Console.ReadLine());
+    int res = int.Parse(Console.ReadLine()!);
     return res;
 }
+
+barray = ChangeSign(barray);
+Console.Write("потом ["+String.Join(", ",(barray))+"]");
