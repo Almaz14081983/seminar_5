6621981d3a35b929ee7e52c9d68d21769145df4c
Almaz <minnexanov.a@mail.ru>
2023-02-12 00:50:49 +0300

    seminar5Task34b

---
+++ seminar5DZTask34b/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+
+int size = InputNumber("Введите размер массива");
+int min = InputNumber("Введите минимальное значение в массиве");
+int max = InputNumber("Введите максимальное значение в массиве");
+int[] array = GetArray(size, min, max );
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] res = new int [size];
+    for(int i=0; i<size; i++)
+    {
+        res[i] = new Random().Next(min, max+1);
+    }
+    return res;    
+}
+
+array = GetArray(size, min, max );
+// Console.WriteLine("сгенерированный массив из случайных ["+String.Join(", ",(array))+"]");
+
+
+int InputNumber(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine()!);
+    return res;
+}
+
+void PrintArray(int[] someArray)
+{
+    //Выводит массив в консоль в виде [,,]
+  Console.Write("сгенерированный массив из случайных чисел [");
+  for (int i = 0; i < someArray.Length - 1; i++)
+  {
+    Console.Write($"{someArray[i]}, ");
+  }
+  Console.WriteLine($"{someArray[someArray.Length - 1]}]");
+    
+ //   Console.WriteLine($"четных {ChangeSign(numberOfEven)}")
+
+}
+
+
+
+
+int[] ChangeSign(int[]someArray)
+{
+    int count = 0;
+    int numberOfEven = 0;
+    //int numberOfOdd = 0;
+    for(int i = 0; i<= someArray.Length-1; i++) 
+    { 
+        if (someArray[i]%2==0) numberOfEven = numberOfEven +1 ;        
+    }
+    count = count + 1;
+    Console.WriteLine($"четных {numberOfEven}");
+    return someArray;          
+}
+
+PrintArray(array);
+array = ChangeSign(array);
+//Console.Write("измененные элементов на противоположные ["+String.Join(", ",(array))+"]");
